de626488371e92df755d7d89cbc4476c483d0afd
Stefano Mazzocchi <stefano.mazzocchi@gmail.com>
2007-09-18 00:37:09 +0000

    start a little refactoring of babel to make it more maven friendly
    git-svn-id: http://simile.mit.edu/repository/babel/trunk@8311 983b779f-54db-0310-a2f8-e46c051b0de2

---
+++ interfaces/src/main/java/edu/mit/simile/babel/SerializationFormat.java
@@ -0,0 +1,26 @@
+package edu.mit.simile.babel;
+
+import java.util.Locale;
+
+/**
+ * @author dfhuynh
+ *
+ */
+public interface SerializationFormat {
+	/**
+	 * @param locale
+	 * @return
+	 */
+	public String getLabel(Locale locale);
+	
+	/**
+	 * @param locale
+	 * @return
+	 */
+	public String getDescription(Locale locale);
+	
+	/**
+	 * @return
+	 */
+	public String getMimetype();
+}
