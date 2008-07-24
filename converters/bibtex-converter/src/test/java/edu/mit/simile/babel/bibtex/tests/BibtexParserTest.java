655f513f76f7a9cad71f13e0a926d507ddc94e92
Stefano Mazzocchi <stefano.mazzocchi@gmail.com>
2008-07-24 06:00:25 +0000

    adding the parsing test and more BibTeX samples
    git-svn-id: http://simile.mit.edu/repository/babel/trunk@9404 983b779f-54db-0310-a2f8-e46c051b0de2

--- converters/bibtex-converter/src/test/java/edu/mit/simile/babel/bibtex/tests/BibtexParserTest.java
+++ converters/bibtex-converter/src/test/java/edu/mit/simile/babel/bibtex/tests/BibtexParserTest.java
@@ -1,14 +1,62 @@
 package edu.mit.simile.babel.bibtex.tests;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.apache.log4j.Logger;
-import org.junit.Test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import edu.mit.simile.babel.bibtex.BibtexCleanerReader;
+import edu.mit.simile.babel.bibtex.BibtexUtils;
+import edu.mit.simile.babel.bibtex.internal.BibtexGrammar;
 
 public class BibtexParserTest {
 
     static Logger logger = Logger.getLogger(BibtexParserTest.class);
     
-    @Test()
-    public void testParser() throws Exception {
+    String[] files = {
+        "allstrings.bib",
+        "comments.bib",
+        "dups.bib",
+        "simple.bib",
+        "zotero.bib",
+        "main.bib",
+        "karger.bib",
+        "demaine.bib",
+        "endnote.bib",
+        "edge_cases.bib"
+    };
+    
+    
+    @Test public void testParser() throws Exception {
+        int counter = 0;
+        for (int i = 0; i < files.length; i++) {
+            logger.info("Parsing: " + files[i]);
+            InputStream stream = null;
+            try {
+                stream = this.getClass().getClassLoader().getResourceAsStream(files[i]);
+                BibtexGrammar p = new BibtexGrammar(
+                    new BibtexCleanerReader(
+                        new BufferedReader(
+                            BibtexUtils.unescapeUnicode(
+                                new InputStreamReader(stream, "US-ASCII")
+                            )
+                        )
+                    )
+                );
+                p.parse();
+                p.printout();
+                counter++;
+            } catch (Exception e) {
+                logger.error(e);
+                stream.close();
+            }
+        }
+        
+        assertTrue(counter == files.length);
     }
-
+    
 }
